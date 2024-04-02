7e3df1a22e64be60fe9622dc4ba3d3fce37725a6
DESKTOP-GB1STEO\AdminSudo <camtv.frmobile@gmail.com>
2024-04-02 17:09:25 +0700

    update

--- ActiveSublimeText/Program.cs
+++ ActiveSublimeText/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ActiveSublimeText
@@ -26,8 +23,23 @@ namespace ActiveSublimeText
                 foreach (Process process in sublimes) { process.Kill(); }
                 // active
                 string hexexe = ReadFileAsHexString(sublime);
-                hexexe = hexexe.Replace("97 94 0D", "00 00 00");
-                hexexe = hexexe.Replace("80 78 05 00 0F 94 C1", "C6 40 05 01 48 85 C9");
+                hexexe = hexexe.Replace("97 94 0D", "00 00 00"); // License for TwitterInc
+                /*
+                 ----- BEGIN LICENSE -----
+                TwitterInc
+                200 User License
+                EA7E-890007
+                1D77F72E 390CDD93 4DCBA022 FAF60790
+                61AA12C0 A37081C5 D0316412 4584D136
+                94D7F7D4 95BC8C1C 527DA828 560BB037
+                D1EDDD8C AE7B379F 50C9D69D B35179EF
+                2FE898C4 8E4277A8 555CE714 E1FB0E43
+                D5D52613 C3D12E98 BC49967F 7652EED2
+                9D2D2E61 67610860 6D338B72 5CF95C69
+                E36B85CC 84991F19 7575D828 470A92AB
+                ------ END LICENSE ------
+                 */
+                hexexe = hexexe.Replace("80 78 05 00 0F 94 C1", "C6 40 05 01 48 85 C9"); // Unlimited User License
                 WriteHexStringAsBinaryToFile(hexexe, sublime);
                 // finish
                 Process.Start(sublime);
