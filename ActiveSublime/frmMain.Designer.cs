aa76d73be03370530efbdba9112bfde03e29173f
DESKTOP-R1SMO72\campx <toisekhongcamchiu@gmail.com>
2025-04-17 22:05:48 +0700

    add ver 4192

--- ActiveSublime/frmMain.Designer.cs
+++ ActiveSublime/frmMain.Designer.cs
@@ -34,8 +34,8 @@
             // 
             // buttonForBuild4169
             // 
-            this.buttonForBuild4169.Location = new System.Drawing.Point(181, 13);
-            this.buttonForBuild4169.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonForBuild4169.Location = new System.Drawing.Point(13, 61);
+            this.buttonForBuild4169.Margin = new System.Windows.Forms.Padding(4);
             this.buttonForBuild4169.Name = "buttonForBuild4169";
             this.buttonForBuild4169.Size = new System.Drawing.Size(176, 40);
             this.buttonForBuild4169.TabIndex = 1;
@@ -48,9 +48,9 @@
             this.buttonForBuild4180.Location = new System.Drawing.Point(13, 13);
             this.buttonForBuild4180.Margin = new System.Windows.Forms.Padding(4);
             this.buttonForBuild4180.Name = "buttonForBuild4180";
-            this.buttonForBuild4180.Size = new System.Drawing.Size(150, 40);
+            this.buttonForBuild4180.Size = new System.Drawing.Size(160, 40);
             this.buttonForBuild4180.TabIndex = 0;
-            this.buttonForBuild4180.Text = "for Build 4180";
+            this.buttonForBuild4180.Text = "for Build 4180, 4192";
             this.buttonForBuild4180.UseVisualStyleBackColor = true;
             this.buttonForBuild4180.Click += new System.EventHandler(this.buttonForBuild4180_Click);
             // 
@@ -58,11 +58,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(370, 61);
+            this.ClientSize = new System.Drawing.Size(370, 106);
             this.Controls.Add(this.buttonForBuild4180);
             this.Controls.Add(this.buttonForBuild4169);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMain";
             this.Text = "Active SublimeText";
             this.Load += new System.EventHandler(this.frmMain_Load);
