b6ffdd722c4cc560a8f89bc8273ef2547d652b7f
DESKTOP-R1SMO72\campx <toisekhongcamchiu@gmail.com>
2026-06-12 18:16:36 +0700

    update

--- MediaPlayerRandom/Form1.cs
+++ MediaPlayerRandom/Form1.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace MediaPlayerRandom
 {
     public partial class Form1 : Form
     {
-        static string mediaFolder = string.Empty;
+        List<string> mediaFiles = new List<string>();
+        int index = 0;
 
         public Form1()
         {
@@ -30,7 +33,10 @@ namespace MediaPlayerRandom
 
             if (dlg.ShowDialog() == DialogResult.OK)
             {
-                mediaFolder = Path.GetDirectoryName(dlg.FileName);
+                string mediaFolder = Path.GetDirectoryName(dlg.FileName);
+                mediaFiles = Directory.GetFiles(mediaFolder, "*.mp4").ToList();
+                //mediaFiles.RandomList();
+                mediaFiles.Shuffle();
             }
             else
             {
@@ -40,10 +46,40 @@ namespace MediaPlayerRandom
 
         private void buttonRandom_Click(object sender, EventArgs e)
         {
-            string[] mediaFiles = Directory.GetFiles(mediaFolder, "*.mp4");
-            int index = new Random().Next(0, mediaFiles.Length);
+            Process.Start(mediaFiles[index++]);
 
-            Process.Start(mediaFiles[index]);
+            if (index >= mediaFiles.Count)
+            {
+                index = 0;
+            }
+        }
+    }
+
+    public static class ListExtensions
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this List<T> list)
+        {
+            int n = list.Count;
+
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+
+        public static List<T> RandomList<T>(this List<T> input, int take = 0)
+        {
+            Random rnd = new Random();
+            List<T> list = (from item in input orderby rnd.Next() select item).ToList<T>();
+            return list.Take((take == 0) ? list.Count : take).ToList<T>();
         }
 
     }
