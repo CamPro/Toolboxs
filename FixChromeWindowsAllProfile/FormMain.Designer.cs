f40fe058bbf8ef53552b2e25c6d6c50a07df8e86
DESKTOP-R1SMO72\campx <toisekhongcamchiu@gmail.com>
2026-07-22 17:30:21 +0700

    update

---
+++ FixChromeWindowsAllProfile/FormMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace FixChromeWindowsAllProfile
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(65, 37);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(200, 80);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start Fix";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 161);
+            this.Controls.Add(this.buttonStart);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormMain";
+            this.Text = "Fix Chrome Windows All Profile";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ FixChromeWindowsAllProfile/FormMain.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace FixChromeWindowsAllProfile
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string userDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Google", "Chrome", "User Data");
+
+            string[] files = Directory.GetFiles(userDataFolder, "Preferences", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                if (file.Contains("Guest Profile") || file.Contains("System Profile"))
+                {
+                    continue;
+                }
+
+                string jsontext = File.ReadAllText(file);
+
+                JObject objs = JObject.Parse(jsontext);
+
+                objs["browser"]["window_placement"]["left"] = 0;
+                objs["browser"]["window_placement"]["top"] = 0;
+                objs["browser"]["window_placement"]["right"] = Screen.PrimaryScreen.Bounds.Width;
+                objs["browser"]["window_placement"]["bottom"] = Screen.PrimaryScreen.Bounds.Height - 40;
+                objs["browser"]["window_placement"]["maximized"] = true;
+
+                jsontext = objs.ToString();
+
+                File.WriteAllText(file, jsontext);
+            }
+
+            MessageBox.Show("Done");
+
+            Application.Exit();
+        }
+    }
+}
